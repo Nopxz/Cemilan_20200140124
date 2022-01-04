3e3d908ab1618ba1f0d3c808ab320744d80a4325
Naufal Gita Mahardika <96105558+Nopxz@users.noreply.github.com>
2022-01-04 10:36:37 +0700

    Add files via upload

---
+++ src/main/java/Cemilan/Java/Cemilan/Java/Final/Application.java
@@ -0,0 +1,13 @@
+package Cemilan.Java.Cemilan.Java.Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/Cemilan/Java/Cemilan/Java/Final/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Cemilan.Java.Cemilan.Java.Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
